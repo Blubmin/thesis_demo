6caf40520a7046b10680e4495a3e9ad84757a26b
Blubmin <imeeder@cox.net>
2019-08-15 18:21:40 -0700

    The camera now solves to follow the bunny at a fixed radius.

--- src/aesthetic_camera_component.cpp
+++ src/aesthetic_camera_component.cpp
@@ -19,12 +19,12 @@ class AestheticCostFunction {
     if (target_ptr == nullptr) {
       return false;
     }
-    Eigen::Vector4<T> target_pos = Eigen::Vector4d(0, 0, 0, 1).cast<T>();
+    Eigen::Vector4<T> target_pos = Eigen::Vector4d(0, 1, 0, 1).cast<T>();
     target_pos = target_ptr->GetTransform().cast<T>() * target_pos;
 
-    // Eigen::Vector3<T> axis(parameters[0], parameters[1], parameters[2]);
-    // Eigen::AngleAxis<T> angle_axis(axis.norm(), axis.normalized());
-    Eigen::Vector3<T> camera_pos(parameters[0], T(0.0), parameters[1]);
+    Eigen::Vector3<T> axis(parameters[0], parameters[1], parameters[2]);
+    Eigen::AngleAxis<T> angle_axis(axis.norm(), axis.normalized());
+    Eigen::Vector3<T> camera_pos(parameters[0], parameters[1], parameters[2]);
     // Eigen::Vector3<T> camera_pos(parameters[3], parameters[4],
     // parameters[5]);
 
@@ -39,16 +39,26 @@ class AestheticCostFunction {
     Eigen::Vector4<T> norm_pos = camera_ptr->GetPerspective().cast<T>() *
                                  camera_T_world.matrix() * target_pos;
 
-    residuals[0] = (Eigen::Vector2<T>::Zero() -
-                    Eigen::Vector2<T>(norm_pos.x(), norm_pos.y()))
-                       .norm();
+    Eigen::Vector3<T> position_diff =
+        target_pos.head<3>() -
+        Eigen::Vector3<T>(parameters[0], parameters[1], parameters[2]);
+    residuals[0] = position_diff[0];
+    residuals[1] = position_diff[1];
+    residuals[2] = position_diff[2];
+
+    residuals[3] = ceres::pow(position_diff.norm() - 7.0, 2);
+
+    residuals[4] = norm_pos[0];
+    residuals[5] = norm_pos[1];
+
+    LOG(INFO) << target_pos.head<3>().transpose();
     return true;
   }
 
   static ceres::CostFunction* Create(std::weak_ptr<pxl::Camera> camera,
                                      std::weak_ptr<pxl::Entity> entity) {
-    ceres::AutoDiffCostFunction<AestheticCostFunction, 1, 2>* cost_function =
-        new ceres::AutoDiffCostFunction<AestheticCostFunction, 1, 2>(
+    ceres::AutoDiffCostFunction<AestheticCostFunction, 6, 3>* cost_function =
+        new ceres::AutoDiffCostFunction<AestheticCostFunction, 6, 3>(
             new AestheticCostFunction(camera, entity));
     return cost_function;
   }
@@ -75,17 +85,17 @@ void AestheticCameraComponent::Update(float time_elapsed) {
   // parameters[4] = camera_ptr->position.y();
   // parameters[5] = camera_ptr->position.z();
   parameters[0] = camera_ptr->position.x();
-  // parameters[1] = camera_ptr->position.y();
-  parameters[1] = camera_ptr->position.z();
+  parameters[1] = camera_ptr->position.y();
+  parameters[2] = camera_ptr->position.z();
 
   auto cost = AestheticCostFunction::Create(camera_ptr, target_);
   problem.AddResidualBlock(cost, NULL, parameters.data());
 
   ceres::Solver::Options options;
-  options.num_threads = 1;
-  options.max_num_iterations = 50;
-  options.parameter_tolerance = 1e-6;
-  options.function_tolerance = 1e-6;
+  options.num_threads = 12;
+  options.max_num_iterations = 100;
+  // options.parameter_tolerance = 1e-6;
+  // options.function_tolerance = 1e-6;
   options.linear_solver_type = ceres::LinearSolverType::DENSE_QR;
 
   ceres::Solver::Summary summary;
@@ -95,9 +105,10 @@ void AestheticCameraComponent::Update(float time_elapsed) {
     LOG(ERROR) << summary.FullReport();
     return;
   }
-  LOG(ERROR) << summary.FullReport();
   camera_ptr->position =
-      Eigen::Vector3f(parameters[0], camera_ptr->position.y(), parameters[1]);
+      Eigen::Vector3f(parameters[0], parameters[1], parameters[2]);
+  LOG(ERROR) << camera_ptr->position.transpose();
+  LOG(ERROR) << summary.FullReport();
 }
 
 void AestheticCameraComponent::SetTarget(std::weak_ptr<pxl::Entity> target) {
